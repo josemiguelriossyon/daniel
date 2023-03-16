702bb20dc7ea7fe82664698828bb5865bddb0d4a
Daniel <daniel.villa@syon.com>
2023-03-16 10:12:16 +0100

    Lista de la compra

---
+++ AngularJSForm/ControllersNg/myCtrl.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("myShoppingList", []).controller("myCtrl", function ($scope) {
+    $scope.products = ["Leche", "Pan", "Queso"];
+    $scope.addItem = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) { return; }
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.products.splice(x, 1);
+    }
+});
